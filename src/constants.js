6f80032a2a7cad66164d4af0293b13a6ca21eb51
RohanSatram <satram.rohan@gmail.com>
2025-02-28 00:31:21 +0530

    V1

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work on AI and back-end development, mostly on Python.
+      I've made a couple of projects in that language. I like making stuff that people would actually use.
+      Here is my <a href="https://github.com/rohansatram" target="_blank">Github</a>!`,
+  "cs-degree": `This is my AIML degree.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch shows, or play video games. Some of my favourite shows are Dark, Severance, Mr. Robot and Game of Thrones.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+     <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>, and
+    <a href="https://www.youtube.com/@Fireship" target="_blank">Fireship</a>`,
+  bed: `This where I sleep.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/RohanSatram/Resume/blob/main/Resume.pdf" target="_blank">Check it out?</a>
+    Contact me at satram.rohan@gmail.com if you have any interesting job opportunities!`,
+  projects: `You can check out my GitHub to see my projects like YouTube Summariser, AI Workout Assistant, Maze Solver, <a href="https://github.com/rohansatram" target="_blank">here</a>.`,
+  library: `There are a lot of science-fiction books in my library, a few of my favourites are Dune, The Hitchhiker's Guide to the Galaxy and Exhalation by Ted Chiang.`,
+  exit: `If you want to exit Rohan's portfolio, just close the tab.`,
+};
