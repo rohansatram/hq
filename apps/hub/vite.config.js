6387f44064ed0a37ecf9ac78442ab4da115e3f96
RohanSatram <satram.rohan@gmail.com>
2026-07-25 20:52:08 +0530

    add work & personal sections and more cards

--- apps/hub/vite.config.js
+++ apps/hub/vite.config.js
@@ -1,7 +1,17 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
     port: 3000
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        uses: resolve(__dirname, "uses.html")
+      }
+    }
   }
 });
